35a915c9d84cb48aef9000c05dec3288ae5ac5aa
sabareesh2007 <sabareeshu2007@gmail.com>
2025-12-16 13:58:02 +0530

    Secured database credentials

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 // --- CONFIGURATION ---
 const JWT_SECRET = 'secret_key_123';
 // ⚠️ PASTE YOUR MONGODB CONNECTION STRING HERE
-const dbURL = 'mongodb+srv://sabareeshu2007_db_user:Sabareesh%242007@cluster0.nanwaap.mongodb.net/?appName=Cluster0';
+const dbURL = process.env.MONGO_URL; // Read the secret key
 
 mongoose.connect(dbURL)
 .then(() => console.log("✅ Database Connected"))
